1ab59c29441aec71d0c861732044c2528ef62cbf
euronym <brunos-martins@hotmail.com>
2022-01-05 14:54:19 -0300

    reestruturação dos diretórios e adição de dados das doenças

--- server/Routes/leaves.py
+++ server/Routes/leaves.py
@@ -1,19 +1,19 @@
-import base64
 import numpy as np
 from tensorflow import keras
-from flask import Flask, request, abort, redirect, url_for, jsonify
-import io, sys
+from flask import Flask, request, jsonify
+import sys
 import cv2
 import json
 import requests
+# tensorflow serving URL
+url = 'http://localhost:8501/v1/models/leaves_classifier:predict'
 
 app = Flask(__name__)
 
-#server URL
-url = 'http://localhost:8501/v1/models/leaves_classifier:predict'
-
-def get_disease():
-
+def check_result(predictions):
+    result = np.amax(predictions)
+    disease = predictions.index(result)
+    if predictions
 def make_prediction():
     img = keras.preprocessing.image.load_img('test.jpeg', target_size=(256, 256))
     x = keras.preprocessing.image.img_to_array(img)
@@ -23,8 +23,9 @@ def make_prediction():
     data = json.dumps({"instances": image.tolist()})
     json_response = requests.post(url, data=data, headers=headers)
     predictions = json.loads(json_response.text)['predictions']
-
     return predictions
+
+
 @app.route("/leaf", methods=['post'])
 def upload_file():
     file = request.files['image'].read()
@@ -36,12 +37,12 @@ def upload_file():
 
 @app.after_request
 def after_request(response):
-    print("log: setting cors" , file = sys.stderr)
+    print("log: setting cors", file=sys.stderr)
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,POST')
     return response
 
-if __name__ == "__main__":
-	app.run()
 
+if __name__ == "__main__":
+    app.run()
