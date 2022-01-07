9f917da609de4cf6a01450dd3739daf24287cd4e
euronym <brunos-martins@hotmail.com>
2022-01-06 23:30:35 -0300

    retorno ao usuário de dados detalhados de cada doença

--- server/Routes/leaves.py
+++ server/Routes/leaves.py
@@ -11,9 +11,30 @@ url = 'http://localhost:8501/v1/models/leaves_classifier:predict'
 app = Flask(__name__)
 
 def check_result(predictions):
+
+    f = open('diseases.json', encoding='utf8')
+    diseases_data = json.load(f)
     result = np.amax(predictions)
-    disease = predictions.index(result)
-    if predictions
+    disease = predictions.tolist().index(result)
+
+    if disease == 0:
+        predicted_disease = diseases_data['first_disease']
+    elif disease == 1:
+        predicted_disease = diseases_data['second_disease']
+    elif disease == 2:
+        predicted_disease = 'healthy'
+    elif disease == 3:
+        predicted_disease = diseases_data['third_disease']
+    elif disease == 4:
+        predicted_disease = diseases_data['fourth_disease']
+    elif disease == 5:
+        predicted_disease = diseases_data['fifth_disease']
+    else:
+        predicted_disease = 0
+    f.close()
+
+    response = {"accuracy": result, "disease_info": predicted_disease}
+    return response
 def make_prediction():
     img = keras.preprocessing.image.load_img('test.jpeg', target_size=(256, 256))
     x = keras.preprocessing.image.img_to_array(img)
@@ -23,9 +44,9 @@ def make_prediction():
     data = json.dumps({"instances": image.tolist()})
     json_response = requests.post(url, data=data, headers=headers)
     predictions = json.loads(json_response.text)['predictions']
-    return predictions
-
-
+    predictions = np.reshape(predictions, -1)
+    response = check_result(predictions)
+    return response
 @app.route("/leaf", methods=['post'])
 def upload_file():
     file = request.files['image'].read()
@@ -33,8 +54,7 @@ def upload_file():
     img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
     cv2.imwrite('test.jpeg', img)
     prediction = make_prediction()
-    return jsonify({'result': prediction})
-
+    return jsonify(result=prediction)
 @app.after_request
 def after_request(response):
     print("log: setting cors", file=sys.stderr)
